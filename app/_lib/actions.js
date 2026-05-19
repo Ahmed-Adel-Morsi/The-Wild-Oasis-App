5ef76804b259eb86e80b8dd73271bac038e4f2ba
Ahmed Adel <ahmedadel0239@gmail.com>
2026-05-19 06:22:49 +0300

    add createReservation action function to handle reservation creation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -28,20 +28,45 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 
-export async function deleteReservation(bookingId) {
+export async function createReservation(reservationData, formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const newReservation = {
+    ...reservationData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: reservationData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  };
+
+  const { error } = await supabase.from("bookings").insert([newReservation]);
+
+  if (error) throw new Error("Booking could not be created");
+
+  revalidatePath(`/cabins/${reservationData.cabinId}`);
+
+  redirect("/cabins/thankyou");
+}
+
+export async function deleteReservation(reservationId) {
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
   const guestBookings = await getBookings(session.user.guestId);
   const guestBookingsIds = guestBookings.map((booking) => booking.id);
 
-  if (!guestBookingsIds.includes(bookingId))
+  if (!guestBookingsIds.includes(reservationId))
     throw new Error("Your are not allowed to delete this booking");
 
   const { error } = await supabase
     .from("bookings")
     .delete()
-    .eq("id", bookingId);
+    .eq("id", reservationId);
 
   if (error) throw new Error("Booking could not be deleted");
 
